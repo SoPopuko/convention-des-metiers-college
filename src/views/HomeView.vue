ab3dd63acebd2eea817221427ee128a117f630c4
sophia <laloux.sophia@gmail.com>
2024-01-03 15:15:01 +0100

    add FAQ page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <FAQ />
+  </div>
 </template>
+
+
+<script setup lang="ts">
+import FAQ from '../components/faq.vue'
+</script>
