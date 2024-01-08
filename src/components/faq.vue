51035f32e42ef2e5130fcad3e25222f4e0fabed2
sophia <laloux.sophia@gmail.com>
2024-01-08 11:52:18 +0100

    add faq page

--- src/components/faq.vue
+++ src/components/faq.vue
@@ -1,5 +1,69 @@
 <template>
-  <div class="item">
-    hello
+  <div>
+      // <test :question="'Pourquoi ce métier ?'" :reponse="'Parce que.'" />
+      <div>
+          <div class="question">
+            Pourquoi ce métier ?
+          </ div>
+
+          <div class="reponse">
+            Car on peut créer tout ce que l'on veut ! Si une application ne te plait pas car il manque des fonctionnalités, tu peux les faire toi meme !
+            <br/>
+            Aussi, c'est un métier d'avenir avec toute cette technologie qui nous entoure !
+          </ div>
+      </div>
+<br/><br/>
+      <div>
+          <div class="question">
+            De quelles compétences ai-je besoin ?
+          </ div>
+
+          <div class="reponse">
+            Essentiellement de logique, de pas mal de créativité et surtout: aimer résoudre des problèmes !
+          </ div>
+      </div>
+<br/><br/>
+      <div>
+          <div class="question">
+            Par quoi commencer ?
+          </ div>
+
+          <div class="reponse">
+            Tu peux commencer par apprendre le langage C car il permet de comprendre les bases du développement.
+            <br/>
+            Par la suite, tu peux apprendre différents langages comme le Python ou le Javascript
+          </ div>
+      </div>
+<br/><br/>
+      <div>
+          <div class="question">
+            Des conseils de sites ou séries à voir ?
+          </ div>
+
+          <div class="reponse">
+            Serie:<br/>
+            - Mister Robot (Netflix)
+            <br/>
+            Sites et jeux:<br/>
+            - Check.io<br/>
+            - CSS Diner<br/>
+            - Code combat<br/>
+            - Elevator Saga<br/>
+            - Robocode<br/>
+            - Codewars<br/>
+            - OverTheWire<br/>
+            - W3School<br/>
+
+            <br/>
+            Applications:<br/>
+            - Scratch (PC)<br/>
+            - Mimo (Mobile)
+          </ div>
+      </div>
+
   </div>
 </ template>
+
+// <script setup lang="ts">
+// import test from './question.vue'
+// </script>
---
+++ src/components/question.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <div class="question">
+        {{ questionData }}
+      </ div>
+
+      <div class="reponse">
+        {{ reponseData }}
+      </ div>
+  </div>
+</template>;
+
+<script lang="js">
+export default {
+    data() {
+        return {
+            questionData: question,
+            reponseData: reponse
+        }
+  },
+  props() {
+    question: String,
+    reponse: String
+  }
+}
+</script>
