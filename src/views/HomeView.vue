8a6499ea3fb3ec8c2bb7fb16f8a492985acfb381
sophia <laloux.sophia@gmail.com>
2024-01-11 11:52:07 +0100

    finish CSS on faq vf

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="flex text-xl">
     <FAQ class="w-4/12 mr-4" />
     <PokeGame class="w-4/12 m-auto bg-white" />
-    <VF class="w-4/12 mr-4" />
+    <VF class="w-4/12 ml-4" />
   </div>
 </template>
 
